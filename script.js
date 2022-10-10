5718ba6aadfd337feccb4c93ff7e90f70c1a7c63
Wh1te_Cl0ud <Whiteman.2000@mail.ru>
2022-10-10 21:50:15 +0600

    Linter errors correction

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-const humberger = document.querySelector('#humberger');
-const navMenu = document.querySelector('#nav-wrap');
 const cross = document.querySelector('#close-button');
-const navWBtn = document.getElementsByClassName("nav-w-btn");
+const navWBtn = document.getElementsByClassName('nav-w-btn');
 
-humberger.addEventListener("click", () => {
-  document.getElementById("nav-wrap").style.left = 0;
+humberger.addEventListener('click', () => {
+  document.getElementById('nav-wrap').style.left = 0;
 });
 
-cross.addEventListener("click", () => {
-  document.getElementById("nav-wrap").style.left = '100%';
+cross.addEventListener('click', () => {
+  document.getElementById('nav-wrap').style.left = '100%';
 });
 
-
-for(let item of navWBtn){
-  item.addEventListener("click", () => {
-  document.getElementById("nav-wrap").style.left = '100%';
-});
+for (let i = 0; i < navWBtn.length; i++) {
+  navWBtn[i].addEventListener('click', () => {
+    document.getElementById('nav-wrap').style.left = '100%';
+  });
 }
