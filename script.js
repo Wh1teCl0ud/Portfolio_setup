0538d29b38bffb74678de41df48c966ff9b7e094
Wh1te_Cl0ud <Whiteman.2000@mail.ru>
2022-10-10 21:52:43 +0600

    One more...

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cross = document.querySelector('#close-button');
 const navWBtn = document.getElementsByClassName('nav-w-btn');
+const humberger = document.querySelector('#humberger');
 
 humberger.addEventListener('click', () => {
   document.getElementById('nav-wrap').style.left = 0;
@@ -9,7 +10,7 @@ cross.addEventListener('click', () => {
   document.getElementById('nav-wrap').style.left = '100%';
 });
 
-for (let i = 0; i < navWBtn.length; i++) {
+for (let i = 0; i < navWBtn.length; i + 1) {
   navWBtn[i].addEventListener('click', () => {
     document.getElementById('nav-wrap').style.left = '100%';
   });
