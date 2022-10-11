4bc089ad9305466d696d0afe37f281859763c13f
Wh1te_Cl0ud <Whiteman.2000@mail.ru>
2022-10-11 13:00:42 +0600

    Linters' setup

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const hamburger = document.querySelector('#hamburger');
 const cross = document.querySelector('#close-button');
 let navWBtn = document.getElementsByClassName('nav-w-btn');
-// navWBtn = Array.from(navWBtn);
 
 hamburger.addEventListener('click', () => {
   document.getElementById('nav-wrap').style.left = 0;
@@ -10,12 +9,6 @@ hamburger.addEventListener('click', () => {
 cross.addEventListener('click', () => {
   document.getElementById('nav-wrap').style.left = '100%';
 });
-
-// navWBtn.forEach(element => {
-//   element.addEventListener('click', () => {
-//     document.getElementById('nav-wrap').style.left = '100%';
-//   });
-// });
 
 for (let i = 0; i < navWBtn.length; i += 1) {
   navWBtn[i].addEventListener('click', () => {
